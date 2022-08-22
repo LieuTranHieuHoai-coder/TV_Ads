d5e7d9efec21669b0cd5fef5d7295612a3b69fe8
hieuhoai <hieuhoai@IT-HIEUHOAI>
2022-08-22 15:27:36 +0700

    version 1

---
+++ Project/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer2 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer3 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.AllowDrop = true;
+            this.axWindowsMediaPlayer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(-5, 0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(1080, 0);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.UseWaitCursor = true;
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
+            this.axWindowsMediaPlayer1.DoubleClickEvent += new AxWMPLib._WMPOCXEvents_DoubleClickEventHandler(this.axWindowsMediaPlayer1_DoubleClickEvent);
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // axWindowsMediaPlayer2
+            // 
+            this.axWindowsMediaPlayer2.AllowDrop = true;
+            this.axWindowsMediaPlayer2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axWindowsMediaPlayer2.Enabled = true;
+            this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(-5, 630);
+            this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
+            this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(1080, 10);
+            this.axWindowsMediaPlayer2.TabIndex = 1;
+            this.axWindowsMediaPlayer2.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer2_PlayStateChange);
+            this.axWindowsMediaPlayer2.Enter += new System.EventHandler(this.axWindowsMediaPlayer2_Enter);
+            // 
+            // axWindowsMediaPlayer3
+            // 
+            this.axWindowsMediaPlayer3.AllowDrop = true;
+            this.axWindowsMediaPlayer3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axWindowsMediaPlayer3.Enabled = true;
+            this.axWindowsMediaPlayer3.Location = new System.Drawing.Point(-5, 1280);
+            this.axWindowsMediaPlayer3.Name = "axWindowsMediaPlayer3";
+            this.axWindowsMediaPlayer3.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer3.OcxState")));
+            this.axWindowsMediaPlayer3.Size = new System.Drawing.Size(1080, 0);
+            this.axWindowsMediaPlayer3.TabIndex = 2;
+            this.axWindowsMediaPlayer3.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer3_PlayStateChange);
+            this.axWindowsMediaPlayer3.Enter += new System.EventHandler(this.axWindowsMediaPlayer3_Enter);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(-7, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1071, 1061);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(65, 981);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1064, 1061);
+            this.Controls.Add(this.axWindowsMediaPlayer2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.axWindowsMediaPlayer3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Project/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public List<string> videos = new List<string>();
+        public List<string> images = new List<string>();
+        public int count = 0;
+        public int countImg = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            hide_taskBar();
+            
+            set_Width_Height();
+            createListVideos();
+            axWindowsMediaPlayer1.Dock = DockStyle.None;
+            axWindowsMediaPlayer1.enableContextMenu = false;
+            axWindowsMediaPlayer1.enableContextMenu = false;
+            
+            //axWindowsMediaPlayer1.stretchToFit = true;
+            axWindowsMediaPlayer2.Dock = DockStyle.None;
+            axWindowsMediaPlayer2.enableContextMenu = false;
+            //axWindowsMediaPlayer2.stretchToFit = true;
+            axWindowsMediaPlayer3.Dock = DockStyle.None;
+            axWindowsMediaPlayer3.enableContextMenu = false;
+            //axWindowsMediaPlayer3.stretchToFit = true;
+
+
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Escape:
+                    DialogResult dialogResult = MessageBox.Show("Do you want to close program ?", "Exit", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        Application.Exit();
+                        Close();
+                    }
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        this.DialogResult = DialogResult.Cancel;
+                    }
+                    return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        public void hide_taskBar()
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+        public void set_Width_Height()
+        {
+            Rectangle resolution = Screen.PrimaryScreen.Bounds;
+            int w = resolution.Width;
+            int h = resolution.Height;
+            this.Size = new Size(w, h);
+            axWindowsMediaPlayer1.Width = w ;
+            axWindowsMediaPlayer1.Location = new Point(0);
+            axWindowsMediaPlayer1.Height = (h / 3) - (h * 5 / 100);
+            axWindowsMediaPlayer2.Width = w ;
+            axWindowsMediaPlayer2.Location = new Point(0, (h / 3));
+            axWindowsMediaPlayer2.Height = (h / 3) - (h * 5 / 100);
+            axWindowsMediaPlayer3.Width = w;
+            axWindowsMediaPlayer3.Location = new Point(0, ((h / 3))*2);
+            axWindowsMediaPlayer3.Height = (h / 3) - (h * 5 / 100);
+
+            pictureBox1.Width = w;
+            pictureBox1.Height = h;
+            pictureBox1.Location = new Point(0,0-(h*3/100));
+
+
+            //pictureBox1.BringToFront();
+            
+            button1.BackColor = Color.Red; 
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(100, Color.Transparent); ;
+            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(100, Color.Transparent); ;
+            button1.Width = 50;
+            button1.Height = 25;
+            button1.Location = new Point(w - 40, 0);
+            button1.Text = "X";
+            button1.BringToFront();
+        }
+        private void createListVideos()
+        {
+            List<string> f = new List<string>();
+            List<string> f1 = new List<string>();
+            //List<string> f1Videos = new List<string>();
+            //string path = @"D:\Files\TVconfig.txt";
+            //StreamReader streamReader = new StreamReader(path);
+            //string data = streamReader.ReadToEnd();
+
+            string basePath = Environment.CurrentDirectory;
+            string relativePath = "TVconfig.txt";
+            
+            string path = Path.GetFullPath(relativePath);
+
+            StreamReader streamReader = new StreamReader(path);
+            string splitResult = streamReader.ReadToEnd();
+            string[] arr = splitResult.Split(',', '"', '\n', '\r');
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!arr[i].Equals(""))
+                {
+                    f.Add(arr[i].ToString());
+
+                    string[] arrf1 = arr[i].Split('-', '"', '\n', '\r');
+                    for (int j = 0; j < arrf1.Length; j++)
+                    {
+                        if (!arrf1[j].Equals(""))
+                        {
+                            f1.Add(arrf1[j].ToString());
+                            //videos.Add(f1[j]);
+                            //images.Add(image[1]);
+                        }
+
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < f1.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    videos.Add(f1[i]);
+                }
+                else
+                {
+                    images.Add(f1[i]);
+                }
+            }
+
+            Console.WriteLine(images[2]);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.ImageLocation = images[2];
+            //pictureBox1.Image = Image.FromFile(images[0]);
+
+            //pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            //videos.Add("D:/Files/video/1.mp4");
+            //videos.Add("D:/Files/video/2.mp4");
+            //videos.Add("D:/Files/video/3.mp4");
+
+        }
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer3.Visible = false;
+            axWindowsMediaPlayer2.Visible = false;
+            //axWindowsMediaPlayer2.SendToBack();
+
+            axWindowsMediaPlayer1.URL = videos[videos.Count - (videos.Count - count)];
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+
+            //load image
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.ImageLocation = images[0];
+        }
+        private void axWindowsMediaPlayer2_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void axWindowsMediaPlayer3_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            // Check Full Full Screen => Set False
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                axWindowsMediaPlayer1.fullScreen = false;
+                if(axWindowsMediaPlayer1.fullScreen == true)
+                {
+                    axWindowsMediaPlayer1.fullScreen = false;
+                }
+            }
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                count++;
+                countImg++;
+                if (count + 1 > videos.Count)
+                {
+                    axWindowsMediaPlayer1.Visible = false;
+                    axWindowsMediaPlayer2.Visible = true;
+                    axWindowsMediaPlayer3.Visible = false;
+                    count = 0;
+                    axWindowsMediaPlayer2.URL = videos[videos.Count - (videos.Count - count)];
+                    axWindowsMediaPlayer2.Ctlcontrols.play();
+
+                }
+                else
+                {
+                    axWindowsMediaPlayer1.Visible = false;
+                    axWindowsMediaPlayer2.Visible = true;
+                    axWindowsMediaPlayer3.Visible = false;
+                    axWindowsMediaPlayer2.URL = videos[videos.Count - (videos.Count - count)];
+
+                    axWindowsMediaPlayer2.Ctlcontrols.play();
+
+                }
+                //load image
+                if (countImg + 1 > images.Count)
+                {
+
+                    countImg = 0;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox1.ImageLocation = images[images.Count - (images.Count - countImg)];
+                }
+                else
+                {
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox1.ImageLocation = images[images.Count - (images.Count - countImg)];
+                }
+            }
+        }
+        private void axWindowsMediaPlayer2_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            // Check Full Full Screen => Set False
+            if (axWindowsMediaPlayer2.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                axWindowsMediaPlayer2.fullScreen = false;
+                if (axWindowsMediaPlayer2.fullScreen == true)
+                {
+                    axWindowsMediaPlayer2.fullScreen = false;
+                }
+            }
+            if (axWindowsMediaPlayer2.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                count++;
+                countImg++;
+                if (count + 1 > videos.Count)
+                {
+                    axWindowsMediaPlayer1.Visible = false;
+                    axWindowsMediaPlayer2.Visible = false;
+                    axWindowsMediaPlayer3.Visible = true;
+                    count = 0;
+                    axWindowsMediaPlayer3.URL = videos[videos.Count - (videos.Count - count)];
+                    axWindowsMediaPlayer3.Ctlcontrols.play();
+                }
+                else
+                {
+                    axWindowsMediaPlayer1.Visible = false;
+                    axWindowsMediaPlayer2.Visible = false;
+                    axWindowsMediaPlayer3.Visible = true;
+                    axWindowsMediaPlayer3.URL = videos[videos.Count - (videos.Count - count)];
+
+                    axWindowsMediaPlayer3.Ctlcontrols.play();
+
+                }
+                //load image
+                if (countImg + 1 > images.Count)
+                {
+
+                    countImg = 0;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox1.ImageLocation = images[images.Count - (images.Count - countImg)];
+                }
+                else
+                {
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox1.ImageLocation = images[images.Count - (images.Count - countImg)];
+                }
+
+            }
+        }
+        private void axWindowsMediaPlayer3_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            // Check Full Full Screen => Set False
+            if (axWindowsMediaPlayer3.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                axWindowsMediaPlayer3.fullScreen = false;
+                if (axWindowsMediaPlayer3.fullScreen == true)
+                {
+                    axWindowsMediaPlayer3.fullScreen = false;
+                }
+            }
+            if (axWindowsMediaPlayer3.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                count++;
+                countImg++;
+                if (count + 1 > videos.Count)
+                {
+                    axWindowsMediaPlayer1.Visible = true;
+                    axWindowsMediaPlayer2.Visible = false;
+                    axWindowsMediaPlayer3.Visible = false;
+                    count = 0;
+                    axWindowsMediaPlayer1.URL = videos[videos.Count - (videos.Count - count)];
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                }
+                else
+                {
+                    axWindowsMediaPlayer1.Visible = true;
+                    axWindowsMediaPlayer2.Visible = false;
+                    axWindowsMediaPlayer3.Visible = false;
+                    axWindowsMediaPlayer1.URL = videos[videos.Count - (videos.Count - count)];
+
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+
+                }
+                //load image
+                if (countImg + 1 > images.Count)
+                {
+
+                    countImg = 0;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox1.ImageLocation = images[images.Count - (images.Count - countImg)];
+                }
+                else
+                {
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox1.ImageLocation = images[images.Count - (images.Count - countImg)];
+                }
+
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Do you want to close program ?", "Exit", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+                Close();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                this.DialogResult = DialogResult.Cancel;
+            }
+        }
+
+        private void axWindowsMediaPlayer1_DoubleClickEvent(object sender, AxWMPLib._WMPOCXEvents_DoubleClickEvent e)
+        {
+            
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
